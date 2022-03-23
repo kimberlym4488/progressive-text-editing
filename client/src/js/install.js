054b47bf19073e46ac6db2369367b7f25b68b544
Kim (she/hers) windows <kimberlym4488@comcast.net>
2022-03-23 14:02:20 -0700

    Pushing up to Heroku

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,7 +5,6 @@ const butInstall = document.getElementById("buttonInstall");
 window.addEventListener("beforeinstallprompt", (event) => {
   // Store the triggered events
   window.deferredPrompt = event;
-
   // Remove the hidden class from the button.
   butInstall.classList.toggle("hidden", false);
 });
@@ -17,13 +16,11 @@ butInstall.addEventListener("click", async () => {
   if (!promptEvent) {
     return;
   }
-
   // Show prompt
   promptEvent.prompt();
 
   // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
-
   butInstall.classList.toggle("hidden", true);
 });
 
